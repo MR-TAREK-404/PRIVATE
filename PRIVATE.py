9992c9ff7fc1fdd030cc9e3be5d019e61ba5cb5c
TAREK <tarek713609@gmail.com>
2024-06-27 22:10:04 +0600

    Create PRIVATE.py

---
+++ PRIVATE.py
@@ -0,0 +1,9 @@
+#খানকীর পোলা ফাইল পারলে চুরি করে দেখা 
+#তোর আব্বু তারেক এর ফাইল নিবি
+#I AM FUCK YOU
+import platform,os,time
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import pers_64
+elif bit == '32bit':
+    import personal_32
